d31b5ea5b59a6226a41879342caedce6eb6d1a93
lealhuber <lea.huber@datazug.ch>
2025-04-25 15:27:03 +0200

    Added vcf filtering templates

---
+++ steps/variant_filtering/templates.py
@@ -0,0 +1,83 @@
+#!/bin/env python3
+from gwf import AnonymousTarget # type: ignore
+import os
+
+def allele_filter(vcf_file, prefix, out_dir):
+    """ Template for filtering out only biallelic sites and removing indels"""
+    inputs = {'file': vcf_file}
+    outputs = {'filtered_file': f'{out_dir}/{prefix}.biallelic.snp.vcf.gz'}
+    options={
+        'cores': 1,
+        'memory': '32g',
+        'walltime': '10:00:00'
+    }
+    spec = f'''
+    echo "START: $(date)"
+	echo "JobID: $SLURM_JOBID"
+    vcftools --gzvcf {vcf_file} \\
+    --remove-indels --min-alleles 2 --max-alleles 2 --remove-filtered-all \\
+    --recode --recode-INFO-all \\
+    --stdout | bgzip -c > {prefix}.biallelic.snp.vcf.gz
+    '''
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+def quality_filter(vcf_file, prefix, out_dir, min_site_DP, max_site_DP, min_SNP_DP, max_SNP_DP):
+    """ Template for filtering out sites passing basic best standart quality filters"""
+    inputs = {'file': vcf_file}
+    outputs = {'filtered_file': f'{out_dir}/{prefix}.filtered.vcf.gz'}
+    options={
+        'cores': 1,
+        'memory': '32g',
+        'walltime': '10:00:00'
+    }
+    spec = f'''
+    echo "START: $(date)"
+	echo "JobID: $SLURM_JOBID"
+    vcftools --gzvcf {vcf_file} \\
+    --minQ 30 --max-missing 0.95 \\
+    --min-meanDP {min_site_DP} --max-meanDP {max_site_DP} \\
+    --minDP {min_SNP_DP} --maxDP {max_SNP_DP} \\
+    --recode --recode-INFO-all \\
+    --stdout | bgzip -c > {prefix}.filtered.vcf.gz
+    '''
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+def vcf_stats(vcf_file, prefix, out_dir):
+    """ Template for vcf stats"""
+    inputs = {'file': vcf_file}
+    outputs = {'allele_frequency': f'{out_dir}/{prefix}.frq',
+               'depth': f'{out_dir}/{prefix}.idepth',
+               'mean_depth': f'{out_dir}/{prefix}.ldepth.mean',
+               'site_quality': f'{out_dir}/{prefix}.lqual',
+               'missing_incv': f'{out_dir}/{prefix}.imiss',
+               'missing_site': f'{out_dir}/{prefix}.lmiss',
+               'heterozygosity': f'{out_dir}/{prefix}.het',
+               'relatedness': f'{out_dir}/{prefix}.relatedness'}
+    options={
+        'cores': 1,
+        'memory': '32g',
+        'walltime': '10:00:00'
+    }
+    spec = f'''
+    echo "START: $(date)"
+	echo "JobID: $SLURM_JOBID"
+    # Count the number of  variants
+    bcftools view -H {vcf_file} | wc -l > {out_dir}/{prefix}.allCounts
+    # calculate allele frequency
+    vcftools --gzvcf {vcf_file} --freq2 --max-alleles 2 --out {out_dir}/{prefix}
+    # mean depth of coverage per individual
+    vcftools --gzvcf {vcf_file} --depth --out {out_dir}/{prefix}
+    # mean depth per site
+    vcftools --gzvcf {vcf_file} --site-mean-depth --out {out_dir}/{prefix}
+    # site quality
+    vcftools --gzvcf {vcf_file} --site-quality --out {out_dir}/{prefix}
+    # proportion of missing data per individual
+    vcftools --gzvcf {vcf_file} --missing-indv --out {out_dir}/{prefix}
+    # proportion of missing data per site
+    vcftools --gzvcf {vcf_file} --missing-site --out {out_dir}/{prefix}
+    # calculate heterozygosity and inbreeding coefficient per individual
+    vcftools --gzvcf {vcf_file} --het --out {out_dir}/{prefix}
+    # caculate relatedness
+    vcftools --gzvcf {vcf_file} --relatedness --out {out_dir}/{prefix}
+    '''
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
