5dd52e672be8b006751c4031451de9cb4a5a201d
lealhuber <lea.huber@datazug.ch>
2025-09-18 16:01:58 +0200

    Improvements

--- steps/variant_filtering/templates.py
+++ steps/variant_filtering/templates.py
@@ -4,60 +4,171 @@ import os
 
 # With all these filters, I shouldn't lose invariant sites, so make sure of that when filtering.
 
-def allele_filter(vcf_file, prefix, out_dir, MAF):
-    """ Template for filtering out only biallelic sites and removing indels"""
+def random_subset(vcf_file, prefix, out_dir):
+    """ Template for getting a random subset from a vcf file for faster stats testing"""
     inputs = {'file': vcf_file}
-    outputs = {'filtered_file': f'{out_dir}/{prefix}.biallelic.snp.vcf.gz'}
+    outputs = {'subset_file': f'{out_dir}/{prefix}.subset.vcf.gz'}
+    options={
+        'cores': 1,
+        'memory': '4g',
+        'walltime': '24:00:00'
+    }
+    spec = f'''
+    echo "START: $(date)"
+    echo "JobID: $SLURM_JOBID"
+    bcftools view {vcf_file} | vcfrandomsample -r 0.01 > {out_dir}/{prefix}.subset.vcf
+    # compress vcf
+    bgzip {out_dir}/{prefix}.subset.vcf
+    # index vcf
+    bcftools index {out_dir}/{prefix}.subset.vcf.gz
+    '''
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+def vcf_stats_subset(vcf_file, sampling_frq, prefix, out_dir):
+    """ Template for vcf stats on a subset of the data.
+    Adapted from https://speciationgenomics.github.io/filtering_vcfs/
+    """
+    inputs = {'file': vcf_file}
+    outputs = {'allele_frequency': f'{out_dir}/{prefix}.frq',
+               'depth': f'{out_dir}/{prefix}.idepth',
+               'mean_depth': f'{out_dir}/{prefix}.ldepth.mean',
+               'site_quality': f'{out_dir}/{prefix}.lqual',
+               'missing_incv': f'{out_dir}/{prefix}.imiss',
+               'missing_site': f'{out_dir}/{prefix}.lmiss',
+               'heterozygosity': f'{out_dir}/{prefix}.het',
+               'relatedness': f'{out_dir}/{prefix}.relatedness'}
+    options={
+        'cores': 1,
+        'memory': '4g',
+        'walltime': '3-00:00:00'
+    }
+    spec = f'''
+    echo "START: $(date)"
+	echo "JobID: $SLURM_JOBID"
+    # make random subset
+    bcftools view {vcf_file} | vcfrandomsample -r {sampling_frq} > {out_dir}/{prefix}.subset.vcf
+    # compress vcf
+    bgzip {out_dir}/{prefix}.subset.vcf
+    # index vcf
+    bcftools index {out_dir}/{prefix}.subset.vcf.gz
+
+    # calculate allele frequency (for sites with max 2 alleles)
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --freq2 --max-alleles 2 --out {out_dir}/{prefix}
+    # mean depth of coverage per individual
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --depth --out {out_dir}/{prefix}
+    # mean depth per site
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --site-mean-depth --out {out_dir}/{prefix}
+    # site quality
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --site-quality --out {out_dir}/{prefix}
+    # proportion of missing data per individual
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --missing-indv --out {out_dir}/{prefix}
+    # proportion of missing data per site
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --missing-site --out {out_dir}/{prefix}
+    # calculate heterozygosity and inbreeding coefficient per individual
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --het --out {out_dir}/{prefix}
+    # caculate relatedness
+    vcftools --gzvcf {out_dir}/{prefix}.subset.vcf --relatedness --out {out_dir}/{prefix}
+    '''
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+def vcf_stats(vcf_file, prefix, out_dir):
+    """ Template for vcf stats. Adapted from https://speciationgenomics.github.io/filtering_vcfs/"""
+    inputs = {'file': vcf_file}
+    outputs = {'allele_frequency': f'{out_dir}/{prefix}.frq',
+               'depth': f'{out_dir}/{prefix}.idepth',
+               'mean_depth': f'{out_dir}/{prefix}.ldepth.mean',
+               'site_quality': f'{out_dir}/{prefix}.lqual',
+               'missing_incv': f'{out_dir}/{prefix}.imiss',
+               'missing_site': f'{out_dir}/{prefix}.lmiss',
+               'heterozygosity': f'{out_dir}/{prefix}.het',
+               'relatedness': f'{out_dir}/{prefix}.relatedness'}
+    options={
+        'cores': 1,
+        'memory': '4g',
+        'walltime': '01:00:00'
+    }
+    spec = f'''
+    echo "START: $(date)"
+	echo "JobID: $SLURM_JOBID"
+    # Count the number of  variants (takes for ever!)
+    # bcftools view -H {vcf_file} | wc -l > {out_dir}/{prefix}.allCounts
+    # calculate allele frequency (for sites with max 2 alleles)
+    vcftools --gzvcf {vcf_file} --freq2 --max-alleles 2 --out {out_dir}/{prefix}
+    # mean depth of coverage per individual
+    vcftools --gzvcf {vcf_file} --depth --out {out_dir}/{prefix}
+    # mean depth per site
+    vcftools --gzvcf {vcf_file} --site-mean-depth --out {out_dir}/{prefix}
+    # site quality
+    vcftools --gzvcf {vcf_file} --site-quality --out {out_dir}/{prefix}
+    # proportion of missing data per individual
+    vcftools --gzvcf {vcf_file} --missing-indv --out {out_dir}/{prefix}
+    # proportion of missing data per site
+    vcftools --gzvcf {vcf_file} --missing-site --out {out_dir}/{prefix}
+    # calculate heterozygosity and inbreeding coefficient per individual
+    vcftools --gzvcf {vcf_file} --het --out {out_dir}/{prefix}
+    # caculate relatedness
+    vcftools --gzvcf {vcf_file} --relatedness --out {out_dir}/{prefix}
+    '''
+    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
+
+
+def allele_filter(vcf_file, prefix, out_dir):
+    """ Template for filtering out multiallelic sites and removing indels,
+    while keeping monomorphic sites for dxy and pi (no maf filter!).
+    For Fst one does not need monomorphic sites but they don't hurt I think/hope."""
+    inputs = {'file': vcf_file}
+    outputs = {'filtered_file': f'{prefix}.monobiallelic.snp.vcf.gz'}
     options={
         'cores': 1,
         'memory': '32g',
-        'walltime': '10:00:00'
+        'walltime': '2-00:00:00'
     }
     spec = f'''
     echo "START: $(date)"
 	echo "JobID: $SLURM_JOBID"
     vcftools --gzvcf {vcf_file} \\
-    --remove-indels --maf {MAF} --min-alleles 2 --max-alleles 2 --remove-filtered-all \\
+    --remove-indels --max-alleles 2 \\
     --recode --recode-INFO-all \\
-    --stdout | bgzip -c > {prefix}.biallelic.snp.vcf.gz
+    --stdout | bgzip -c > {prefix}.monobiallelic.snp.vcf.gz
     '''
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
-def quality_filter(vcf_file, prefix, out_dir, min_qual, max_missing, min_DP, max_DP):
+def quality_filter(vcf_file, prefix, out_dir, max_missing, min_DP, max_DP):
     """ Template for filtering out sites passing basic best standart quality filters"""
     inputs = {'file': vcf_file}
     outputs = {'filtered_file': f'{out_dir}/{prefix}.filtered.vcf.gz'}
     options={
         'cores': 1,
         'memory': '32g',
-        'walltime': '10:00:00'
+        'walltime': '36:00:00'
     }
     spec = f'''
     echo "START: $(date)"
 	echo "JobID: $SLURM_JOBID"
     vcftools --gzvcf {vcf_file} \\
-    --minQ {min_qual} --max-missing {max_missing} \\
+    --max-missing {max_missing} \\
     --min-meanDP {min_DP} --max-meanDP {max_DP} \\
-    --minDP {min_DP} --maxDP {max_DP} \\
+    # because one sample is less deep but overall fine, don't filter genotype depth
+    # --minDP {min_DP} --maxDP {max_DP} \\
     --recode \\
-    --stdout | bgzip -c > {prefix}.filtered.vcf.gz
+    --stdout | bgzip -c > {out_dir}/{prefix}.filtered.vcf.gz
     '''
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
-def bias_filter(vcf_file, prefix, out_dir, min_qual, avgDP, qual_div_avgDP):
+def bias_filter(vcf_file, prefix, out_dir, avgDP, qual_div_avgDP):
     """ Template for filtering out sites with mapping quality bias, position bias and strand bias using bcftools
-    (suggestions by htslib.org/workflow/filter.html)"""
+    (suggestions by htslib.org/workflow/filter.html). Only problem my vcfs don't contain any of those info fields..."""
     inputs = {'file': vcf_file}
     outputs = {'filtered_file': f'{out_dir}/{prefix}.nobias.vcf.gz'}
     options={
         'cores': 1,
         'memory': '32g',
-        'walltime': '10:00:00'
+        'walltime': '2-00:00:00'
     }
     spec = f'''
-    bcftools view -e "QUAL < {min_qual} || DP>2*{avgDP} || MQBZ < -(3.5+4*{qual_div_avgDP}) || \\
+    bcftools view -e "MQBZ < -(3.5+4*{qual_div_avgDP}) || \\
         RPBZ > (3+3*{qual_div_avgDP}) || RPBZ < -(3+3*{qual_div_avgDP}) || FORMAT/SP > (40+{avgDP}/2)" \\
-        -o {out_dir}/{prefix}.nobias.vcf.gz
+        -Oz -o {out_dir}/{prefix}.nobias.vcf.gz {vcf_file}
     '''
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
 
@@ -81,46 +192,5 @@ def HWE_filter(vcf_file, prefix, samples_list, temp_dir, out_dir, stat_dir):
 
     bcftools view -H {out_dir}/{prefix}.HWE.vcf.gz | wc -l \\
     > {stat_dir}/{prefix}.hwe.allCounts
-    '''
-    return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
-
-
-def vcf_stats(vcf_file, prefix, out_dir):
-    """ Template for vcf stats. Adapted from https://speciationgenomics.github.io/filtering_vcfs/"""
-    inputs = {'file': vcf_file}
-    outputs = {'allele_frequency': f'{out_dir}/{prefix}.frq',
-               'depth': f'{out_dir}/{prefix}.idepth',
-               'mean_depth': f'{out_dir}/{prefix}.ldepth.mean',
-               'site_quality': f'{out_dir}/{prefix}.lqual',
-               'missing_incv': f'{out_dir}/{prefix}.imiss',
-               'missing_site': f'{out_dir}/{prefix}.lmiss',
-               'heterozygosity': f'{out_dir}/{prefix}.het',
-               'relatedness': f'{out_dir}/{prefix}.relatedness'}
-    options={
-        'cores': 1,
-        'memory': '32g',
-        'walltime': '10:00:00'
-    }
-    spec = f'''
-    echo "START: $(date)"
-	echo "JobID: $SLURM_JOBID"
-    # Count the number of  variants
-    bcftools view -H {vcf_file} | wc -l > {out_dir}/{prefix}.allCounts
-    # calculate allele frequency (for sites with max 2 alleles)
-    vcftools --gzvcf {vcf_file} --freq2 --max-alleles 2 --out {out_dir}/{prefix}
-    # mean depth of coverage per individual
-    vcftools --gzvcf {vcf_file} --depth --out {out_dir}/{prefix}
-    # mean depth per site
-    vcftools --gzvcf {vcf_file} --site-mean-depth --out {out_dir}/{prefix}
-    # site quality
-    vcftools --gzvcf {vcf_file} --site-quality --out {out_dir}/{prefix}
-    # proportion of missing data per individual
-    vcftools --gzvcf {vcf_file} --missing-indv --out {out_dir}/{prefix}
-    # proportion of missing data per site
-    vcftools --gzvcf {vcf_file} --missing-site --out {out_dir}/{prefix}
-    # calculate heterozygosity and inbreeding coefficient per individual
-    vcftools --gzvcf {vcf_file} --het --out {out_dir}/{prefix}
-    # caculate relatedness
-    vcftools --gzvcf {vcf_file} --relatedness --out {out_dir}/{prefix}
     '''
     return AnonymousTarget(inputs=inputs, outputs=outputs, options=options, spec=spec)
--- steps/variant_filtering/workflow.py
+++ steps/variant_filtering/workflow.py
@@ -1,6 +1,5 @@
 ## declare the workflow
 from gwf import Workflow # type: ignore
-import numpy as np # type: ignore
 import re, os
 from templates import *
 
@@ -19,55 +18,161 @@ os.makedirs(stat_dir, exist_ok=True)
 os.makedirs(temp_dir, exist_ok=True)
 os.makedirs(output_dir, exist_ok=True)
 
-vcf_all = '/faststorage/project/ostrich_thermal/people/leah/Subspecies/steps/variant_calling/Sc_all/outputs/sorted_Aug25.vcf.gz'
+vcf_all = '/faststorage/project/ostrich_thermal/people/leah/Subspecies/steps/variant_calling/Aug25/outputs/sorted_Aug25.vcf.gz'
 
-stat_nrs = gwf.target_from_template(
-    name = 'get_vcf_stats',
-    template=vcf_stats(
+# make random subset of vcf for testing
+vcf_sub = gwf.target_from_template(
+    name = 'random_subset',
+    template = random_subset(
         vcf_file=vcf_all,
-        prefix='pre_filter',
-        out_dir=stat_dir
+        prefix='raw',
+        out_dir=temp_dir
         )
     )
 
+stat_subset = gwf.target_from_template(
+    name = 'get_stats_subset',
+    template=vcf_stats(
+        vcf_file=vcf_sub.outputs['subset_file'],
+        prefix='pre_filter.subset',
+        out_dir=stat_dir
+        )
+    )
 # from this I should get the average DP and QUAL to use in the next steps
-# I will just put in some values for now, but these should be calculated from the vcf stats output
-avg_DP_ind = 20
-avg_DP = 20
-avg_qual = 200
+# average depth is around 30, quality is abyssmal thus I won't filter on it now (also because by design invariant sites have 0 quality and I want to keep them)
+avg_DP = 30
 
 filter_qual = gwf.target_from_template(
     name = 'filter_qual_all',
     template=quality_filter(
-        vcf_file='/faststorage/project/ostrich_thermal/people/leah/Subspecies/steps/variant_calling/Sc_all/outputs/all.vcf',
-        prefix='all',
-        out_dir=temp_dir,
-        min_site_DP=int(avg_DP/3),
-        max_site_DP=int(avg_DP*2),
-        min_SNP_DP=int(avg_DP_ind/3),
-        max_SNP_DP=int(avg_DP_ind*3)
-        )
-    )
-
-filter_bias = gwf.target_from_template(
-    name = 'filter_bias_all',
-    template=bias_filter(
-        vcf_file=filter_qual.outputs['filtered_file'],
-        prefix='all',
-        out_dir=temp_dir,
-        min_qual=20,   
-        avgDP=avg_DP,
-        qual_div_avgDP=avg_DP/20
-        )
-    )
-
-
-# in the end do stats again
-stat_nrs = gwf.target_from_template(
-    name = 'get_vcf_stats',
-    template=vcf_stats(
         vcf_file=vcf_all,
-        prefix='post_filter',
+        prefix='all',
+        out_dir=temp_dir,
+        #min_qual=20,
+        max_missing=0.9,
+        min_DP=int(avg_DP/3),
+        max_DP=int(avg_DP*2),
+        )
+    )
+
+# check what quality filtering did
+# it won't remove much so do it on small subset again
+
+vcf_sub2 = gwf.target_from_template(
+    name = 'subset_filtered',
+    template = random_subset(
+        vcf_file=filter_qual.outputs['filtered_file'],
+        prefix='filtered',
+        out_dir=temp_dir
+        )
+    )
+
+stat_filtered = gwf.target_from_template(
+    name = 'get_filtered_stats',
+    template=vcf_stats(
+        vcf_file=vcf_sub2.outputs['subset_file'],
+        prefix='qualfilter_sub',
         out_dir=stat_dir
         )
     )
+
+# for calculating Fst I also need to remove multiallelic sites and indels (but keep monomorphic sites i.e. no MAF filter)
+filter_multiallelic = gwf.target_from_template(
+    name = 'filter_multiallelic_all',
+    template=allele_filter(
+        vcf_file=filter_qual.outputs['filtered_file'],
+        prefix='all.filtered', # what are you putting in?
+        out_dir=output_dir
+        )
+    )
+  
+# check what allele filtering did
+stat_postallele = gwf.target_from_template(
+    name = 'get_stats_post_allele',
+    template=vcf_stats_subset(
+        vcf_file=filter_multiallelic.outputs['filtered_file'],
+        sampling_frq=0.5,
+        prefix='post_allelefilter',
+        out_dir=stat_dir
+        )
+    )
+
+ # One can also filter by some biases, I'll try but not sure if I will use it
+ # My vcf files don't contain the relevant info fields though, so nevermind for now
+
+""" filter_bias = gwf.target_from_template(
+    name = 'filter_bias_all',
+    template=bias_filter(
+        vcf_file=filter_qual.outputs['filtered_file'],
+        prefix='all.filtered', # what are you putting in?
+        out_dir=temp_dir,
+        #min_qual=20,   
+        avgDP=avg_DP,
+        qual_div_avgDP=avg_DP/20
+        )
+    ) """
+
+# check what bias filtering did
+""" stat_postbias = gwf.target_from_template(
+    name = 'get_stats_post_bias',
+    template=vcf_stats_subset(
+        vcf_file=filter_bias.outputs['filtered_file'],
+        sampling_frq=0.5,
+        prefix='post_biasfilter',
+        out_dir=stat_dir
+        )
+    )
+ """
+
+
+
+# split file into populations for Hardy-Weinberg filter
+blacksamples = {'P1878_107', 'P1878_108', 'P1878_109', 'P1878_110', 'P1878_111', 'P1878_112', 'P1878_113', 'P1878_114', 'P1878_115', 'P1878_116'}
+bluesamples = {'P1878_117', 'P1878_118', 'P1878_119', 'P1878_120', 'P1878_121', 'P1878_122', 'P1878_123', 'P1878_124', 'P1878_125', 'P1878_126'}
+redsamples = {'P1878_127', 'P1878_128', 'P1878_129', 'P1878_130', 'P1878_131', 'P1878_132', 'P1878_133', 'P1878_134', 'P1878_135', 'P1878_136'}
+
+""" HWE_black = gwf.target_from_template(
+    name = 'black_HWE,
+    template=HWE_filter(
+        vcf_file=filter_biallelic.outputs['filtered_file'],
+        prefix='black.biallelic.snp',
+        samples_list=blacksamples
+        temp_dir=temp_dir,
+        out_dir=output_dir,
+        stat_dir=stat_dir
+    )
+) """
+
+""" HWE_blue = gwf.target_from_template(
+    name = 'blue_HWE,
+    template=HWE_filter(
+        vcf_file=filter_biallelic.outputs['filtered_file'],
+        prefix='blue.biallelic.snp',
+        samples_list=bluesamples
+        temp_dir=temp_dir,
+        out_dir=output_dir,
+        stat_dir=stat_dir
+    )
+) """
+
+""" HWE_red = gwf.target_from_template(
+    name = 'red_HWE,
+    template=HWE_filter(
+        vcf_file=filter_biallelic.outputs['filtered_file'],
+        prefix='red.biallelic.snp',
+        samples_list=redsamples
+        temp_dir=temp_dir,
+        out_dir=output_dir,
+        stat_dir=stat_dir
+    )
+) """
+
+# in the end do stats again
+""" stat_nrs = gwf.target_from_template(
+    name = 'get_vcf_stats',
+    template=vcf_stats(
+        vcf_file=HWE_black.outputs['filtered_file'],
+        prefix='Black_post_HWE',
+        out_dir=stat_dir
+        )
+    ) """
