7b6e7f6d6ab55d06ecb5264705cfb457ebc6fe6a
lealhuber <lea.huber@datazug.ch>
2025-12-04 15:56:15 +0100

    no relatives rerun

--- steps/fst/scripts/commands.sh
+++ steps/fst/scripts/commands.sh
@@ -5,10 +5,12 @@
 #SBATCH --time=12:00:00
 
 # run in steps/fst/scripts/
+vcf_ini=/faststorage/project/ostrich_thermal/people/leah/Subspecies/steps/variant_filtering/outputs/allpops.HWE.merged.vcf.gz
 # index if not already indexed
-bcftools index --threads 8 /faststorage/project/ostrich_thermal/people/leah/Subspecies/steps/variant_filtering/outputs/all.filtered.monobiallelic.snp.vcf.gz
+# bcftools index --threads 8 $vcf_ini
+# echo "VCF indexed"
 # make geno file
-python simonmartin_genomics/VCF_processing/parseVCFs.py -i /faststorage/project/ostrich_thermal/people/leah/Subspecies/steps/variant_filtering/outputs/all.filtered.monobiallelic.snp.vcf.gz \
+python simonmartin_genomics/VCF_processing/parseVCFs.py -i $vcf_ini \
     --threads 8 -o ../temp/atp2.HWE.input.geno.gz
 echo "Geno file created"
 # loop through different window sizes
@@ -16,5 +18,7 @@ seq=(10000 25000 50000 100000)
 for w in ${seq[@]}; do
     # step=$(($w/2))
     # echo "Window size: $w, step size: $step"
-    python simonmartin_genomics/popgenWindows.py -w $w -m 500 -g ../temp/atp1.noHWE.input.geno.gz -o ../outputs/atp2.w_$w.csv.gz -f phased -T 8 -p black -p blue -p red --popsFile populations_file.txt
+    python simonmartin_genomics/popgenWindows.py -w $w -m 500 -g ../temp/atp2.HWE.input.geno.gz -o ../outputs/atp2.w_$w.csv.gz -f phased -T 8 -p black -p blue -p red --popsFile populations_file.txt
 done
+echo "All window sizes completed"
+echo "Ended at $(date)"
